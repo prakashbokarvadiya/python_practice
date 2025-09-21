15e22357c487ee03888eb4297732df711c2360fd
thakorhim <117174109+thakorhim@users.noreply.github.com>
2025-09-21 23:32:52 +0530

    Python

--- practice set 3.py
+++ practice set 3.py
@@ -139,3 +139,25 @@ print("Intersection:", intersection)
 
 
 
+#Q15. Implement stack using a class.
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop() if not self.is_empty() else None
+
+    def peek(self):
+        return self.items[-1] if not self.is_empty() else None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+s = Stack()
+s.push(10)
+s.push(20)
+print(s.pop())
+print(s.peek())
